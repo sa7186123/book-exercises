81396451ae4bd811b2d580a24b9b3cf20ca0982f
Tsz-Ming-Wong <tmwong@uw.edu>
2020-01-16 14:44:21 -0800

    week1

--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -2,11 +2,14 @@
 
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
-
+add_three <- function() {
+  
+}
 
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
 # function
-
+ten <- add_three(7)
+print(10)
 
 # Define a function `imperial_to_metric` that takes in two arguments: a number 
 # of feet and a number of inches
