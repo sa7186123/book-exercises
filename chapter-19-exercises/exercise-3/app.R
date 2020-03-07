94038f42ef738c17e36423c3fefa363a8c05a27b
Ming <ming@Tszs-MacBook-Air.local>
2020-03-06 17:09:57 -0800

    week9

--- chapter-19-exercises/exercise-3/app.R
+++ chapter-19-exercises/exercise-3/app.R
@@ -12,11 +12,13 @@ diamonds_sample <- sample_n(diamonds, 1000)
 
 # For convenience store the `range()` of values for the `price` column
 # (of your sample)
+price_range <- range(diamonds_sample$price)
 
 
 # For convenience, get a vector of column names from the `diamonds` data set to
 # use as select inputs
 
+features <-colname(diamonds)
 
 # To help keep the code organized, we'll store some UI elements in variables
 # _before_ defining the UI.
@@ -27,15 +29,28 @@ diamonds_sample <- sample_n(diamonds, 1000)
 # - a label of "Price (in dollars)"
 # - min and max valuesvalue based on the `price_range` calculated above
 # - a current value equal to the price range
+price_input <- SliderInput(inputID ="price_choice",
+                           label = "Price(in dollar)"
+                           min= price_range[1],
+                           max= price_range[2],
+                           value=price_range
+                           )
 
 
 # Define a variable `feature_input` that is a `selectInput()` with the
 # label "Feature of Interest". This dropdown should let the user pick one of
 # the columns of the diamond data set. Use the `carat` column as a default
 # Make sure to set an inputId to reference in your server!
-
+selectnput <- select(inputID ="price_choice",
+                           label = "Feature of Interest"
+                           min= price_range[1],
+                           max= price_range[2],
+                           value=price_range
+)
 
 # Define a UI using a `fluidPage()` layout with the following content:
+my_ui
+
 
   # A `titlePanel` with the title "Diamond Viewer"
 
@@ -52,12 +67,15 @@ diamonds_sample <- sample_n(diamonds, 1000)
 
 # Define a `server` function (with appropriate arguments)
 # This function should perform the following:
-
+my_server <- Function(input_input, output_list) {
+  output_list$
+}
   # Assign a reactive `renderPlot()` function to the outputted 'plot' value
   
     # This function should first filter down the `diamonds_sample` data
     # using the input price range (remember to get both ends)!
-    
+    ggplot(data=filtered_data) +
+      geom_point(mapping= aer(x=input_list$feature_choice, y= price, colou=cut))
 
     # Use the filtered data set to create a ggplot2 scatter plot with the
     # user-select column on the x-axis, and the price on the y-axis,
@@ -67,7 +85,7 @@ diamonds_sample <- sample_n(diamonds, 1000)
     # Finally, if the "trendline" checkbox is selected, you should add (+)
     # a geom_smooth geometry (with `se=FALSE`) to your plot
     # Hint: use an if statement to see if you need to add more geoms to the plot
-
+install.packages("reconect")
   
     # Be sure and return the completed plot!
   
